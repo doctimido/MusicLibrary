58d6a7f775d2ba496a33a6e06f24fd1d65b0ee9a
ian-windows8 <brazil_ian9@hotmail.com>
2014-03-16 19:16:47 -0400

    new utility methods package

---
+++ src/com/waves/library/utils/DbUtils.java
@@ -0,0 +1,15 @@
+package com.waves.library.utils;
+
+public class DbUtils {
+	
+	// String concatenate
+	public static String[] concat(String[] A, String[] B) {
+		   int aLen = A.length;
+		   int bLen = B.length;
+		   String[] C= new String[aLen+bLen];
+		   System.arraycopy(A, 0, C, 0, aLen);
+		   System.arraycopy(B, 0, C, aLen, bLen);
+		   return C;
+		}
+
+}
